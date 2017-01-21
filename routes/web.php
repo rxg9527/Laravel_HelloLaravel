2fb798f02cbd765f882496dd83a90dc0e6d9c92c
Yuen-rMBP <rxg9527@sina.cn>
2017-01-21 10:30:08 +0800

    Finish Signup Route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('help', 'StaticPagesController@help')->name('help');
 Route::get('about', 'StaticPagesController@about')->name('about');
 
+Route::get('signup', 'UsersController@create')->name('signup');
+
 // get('/', 'StaticPagesController@home');
 // get('/help', 'StaticPagesController@help');
 // get('/about', 'StaticPagesController@about');
