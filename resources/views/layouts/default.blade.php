2e550ad847849468bfb67fd9aae1551a10f6d875
Yuen-rMBP <rxg9527@sina.cn>
2017-01-21 10:09:58 +0800

    Laravel 命名路由

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,12 +5,10 @@
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    <!-- @include 是 Blade 提供的视图引用方法，可通过传送一个具体的文件路径名称来引用视图。 -->
     @include('layouts._header')
 
     <div class="container">
       <div class="col-md-offset-1 col-md-10">
-        <!-- 下面的这行代码表示该占位区域将用于显示 content 区块的内容，而 content 区块的内容将由继承自 default 视图的子视图定义。 -->
         @yield('content')
         @include('layouts._footer')
       </div>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,19 @@
 //     return view('welcome');
 // });
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('help', 'StaticPagesController@help');
-Route::get('about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('help', 'StaticPagesController@help')->name('help');
+Route::get('about', 'StaticPagesController@about')->name('about');
 
 // get('/', 'StaticPagesController@home');
 // get('/help', 'StaticPagesController@help');
 // get('/about', 'StaticPagesController@about');
+
+// 如果要使用下面这种方式来渲染 help 链接，则需要先为路由定义 help 路由名称。
+// <li><a href="{{ route('help') }}">帮助</a></li>
+//
+// 在 Laravel 中，我们可以通过在路由后面链式调用 name 方法来为路由指定名称：
+// get('/help', 'StaticPagesController@help')->name('help');
+//
+// 当我们在页面渲染该路由时，route('help') 最终的渲染结果将如下：
+// http://sample.app/help
