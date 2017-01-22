28481cb76b4213bd5fa1ccd7b154ecf8d4dc0680
Yuen-rMBP <rxg9527@sina.cn>
2017-01-22 16:23:48 +0800

    登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,8 +4,40 @@
       <a href="/" id="logo">Sample App</a>
       <nav>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="{{ route('help') }}">帮助</a></li>
-          <li><a href="#">登录</a></li>
+          @if (Auth::check())
+            <li><a href="#">用户列表</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                {{ Auth::user()->name }} <b class="caret"></b>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="{{ route('users.show', Auth::user()->id) }}">个人中心</a></li>
+                <li><a href="#">编辑资料</a></li>
+                <li class="divider"></li>
+                <li>
+                  <a id="logout" href="#">
+                    <!-- 可以看到用户退出登录的按钮实际上是一个表单的提交按钮，在点击退出按钮之后浏览器将向 /logout 地址发送一个 POST 请求。
+                    但由于 RESTful 架构中会使用 DELETE 请求来删除一个资源，当用户退出时，实际上相当于删除了用户登录会话的资源，因此这里的退出操作需要使用 DELETE 请求来发送给服务器。
+                    由于浏览器不支持发送 DELETE 请求，因此我们需要使用一个隐藏域来伪造 DELETE 请求。
+
+                    在 Blade 模板中，我们可以使用 method_field 方法来创建隐藏域。
+                    {{ method_field('DELETE') }}
+
+                    其转化为 HTML 代码如下：
+                    <input type="hidden" name="_method" value="DELETE"> -->
+                    <form action="{{ route('logout') }}" method="POST">
+                      {{ csrf_field() }}
+                      {{ method_field('DELETE') }}
+                      <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                    </form>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          @else
+            <li><a href="{{ route('help') }}">帮助</a></li>
+            <li><a href="{{ route('login') }}">登录</a></li>
+          @endif
         </ul>
       </nav>
     </div>
